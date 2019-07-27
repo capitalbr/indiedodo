bba772cfceab58d1add1f4e16870b1897e132057
Bradley <capitalb@live.com>
2019-07-27 18:14:43 -0500

    Comment out serviceWorker.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+// const express = require("express");
 
 const app = require("./server/server");
 
-// const port = process.env.PORT || 3001;
-app.set('port', process.env.PORT || 8080);
+const port = process.env.PORT || 3001;
+// app.set('port', process.env.PORT || 8080);
 
-// app.listen(3001, () => {
-//   console.log(`Server listening on port ${1662}`);
-// });
-console.log(app.get('port'))
-app.listen(app.get('port'));
+app.listen(3001, () => {
+  console.log(`Server listening on port ${port}`);
+});
+// console.log(app.get('port'))
+// app.listen(app.get('port'));
