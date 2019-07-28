5612b01c2435af77e093e5be3fb5a4e1200b80e8
Bradley <capitalb@live.com>
2019-07-27 20:50:56 -0500

    Changed express/react ports

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 const app = require("./server/server");
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 1662;
 // app.set('port', process.env.PORT || 8080);
 
-app.listen(1662, () => {
-  console.log(`Server listening on port ${1662}`);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 // console.log(app.get('port'))
 // app.listen(app.get('port'));
