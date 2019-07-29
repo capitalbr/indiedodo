927daa279b25ee57964d00d289cc40defe1bb6e5
Zachary <zachjoliver@gmail.com>
2019-07-28 18:18:48 -0700

    Finish models and validations for campaign

---
+++ server/models/Campaign.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CampaignSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  tagline: {
+    type: String,
+    required: true
+  },
+  overview: {
+    type: String,
+    required: true
+  },
+  story: {
+    type: String,
+    required: true
+  },
+  faq: {
+    type: String,
+    required: true
+  },
+  image_url: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  raised: {
+    type: Number,
+    required: true,
+    max: 1000000000
+  },
+  goal: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 1000000000
+  },
+  end_date: {
+    type: Date,
+    required: true,
+  }
+});
+
+module.exports = Campaign = mongoose.model('campaigns', CampaignSchema);
