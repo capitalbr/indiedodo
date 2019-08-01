1294059ee9e621a2e3bc452e73e3fa1dc4ffcd10
Bradley <bbarnes.me@gmail.com>
2019-08-01 18:21:54 -0500

    Complete progress bar and user box

--- client/src/components/campaigns/CampaignShow.jsx
+++ client/src/components/campaigns/CampaignShow.jsx
@@ -3,9 +3,14 @@ import { Query } from "react-apollo";
 import { FaAccusoft } from "react-icons/fa"; 
 
 import Queries from "../../graphql/queries";
-const { FETCH_CAMPAIGN, FETCH_USER, FETCH_CAMPAIGN_CONTRIBUTIONS } = Queries;
+const { FETCH_CAMPAIGN, FETCH_USER, FETCH_CAMPAIGN_CONTRIBUTIONS, FETCH_USER_CAMPAIGNS } = Queries;
 
-const AllContributions = (campaign_id, goal) => {
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const AllContributions = (campaign_id, goal, end_date) => {
+  // debugger
   return (
     <Query
       query={FETCH_CAMPAIGN_CONTRIBUTIONS}
@@ -16,16 +21,39 @@ const AllContributions = (campaign_id, goal) => {
         if (error) return 0;
         const { campaignContributions } = data;
         let total = 0;
+        let numContributions = 0;
+        let backerText = (numContributions > 0) ? "backers" : "backer";
         campaignContributions.forEach((contribution) => {
           total = total + Number(contribution.amount)
+          numContributions = numContributions + 1;
         })
+        let percent_raised = (total / goal * 100).toFixed(2);
+        total = total.toFixed(0);
+        const endDate = new Date(end_date);
+        const  currDate = new Date();
+        const diffTime = Math.abs(endDate.getTime() - currDate.getTime());
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        let dayText = (numContributions > 0) ? "days" : "day";
         return (
           <div>
-            <div>
-              ${total} USD raised
+            <div className="raised-text-cont">
+               <div>
+                  ${numberWithCommas(total)} USD raised
+                </div>
+                <div>
+                {numberWithCommas(numContributions)} {backerText}
+                </div>
             </div>
-            <div>
-              {total / goal * 100}%
+            <div className="raised-bar-cont">
+              <div className="raised-bar" style={{ width: `${percent_raised}%`, maxWidth: "100%" }} />
+            </div>
+            <div className="raised-text-cont">
+              <div>
+                {percent_raised}% of ${numberWithCommas(goal.toFixed(0))} Flexible Goal
+              </div>
+              <div>
+                {diffDays} {dayText} left
+              </div>
             </div>
           </div>
         )
@@ -52,7 +80,7 @@ class CampaignShow extends React.Component {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error</p>;
             this.user = data.campaign.user;
-            this.contributions = AllContributions(this.props.match.params.campaignId, data.campaign.goal);
+            this.contributions = AllContributions(this.props.match.params.campaignId, data.campaign.goal, data.campaign.end_date);
             // { title, tagline, overview, story, faq, image_url, category, goal, end_date } = data.campaign;
             return (
               <div>
@@ -73,12 +101,50 @@ class CampaignShow extends React.Component {
                           if (loading) return <p>Loading...</p>;
                           if (error) return <p>Error</p>;
                           return <div className="campaign-owner">
-                            <div className="user-icon-show"><FaAccusoft/></div>
-                            {data.user.name}
-                            {data.user.bio_header}
+                            <div className="user-icon-show"><FaAccusoft className="user-img"/></div>
+                          <div className="user-icon-show-info">
+                              <div className="user-name">{data.user.name}</div>
+                              <Query
+                                query={FETCH_USER_CAMPAIGNS}
+                                variables={{ userId: this.user }}
+                              >
+                                {({ loading, error, data }) => {
+                                  if (loading) return <p>Loading...</p>;
+                                  if (error) return <p>Error</p>;
+                                  let c;
+                                  if (data.userCampaigns.length > 1) {
+                                    c = "Campaigns"
+                                  } else {
+                                    c = "Campaign"
+                                  }
+                                  return(
+                                    <div className="campaign-amount">
+                                      {data.userCampaigns.length} {c}
+                                    </div>
+                                  )
+                                }}
+                              </Query>
+                            </div>
                           </div>
                       }}
                       </Query>
+                {this.contributions}
+                <div className="campaign-show-header-buttons">
+                  <div className="backit">
+                      <button>Backit</button>
+                  </div>
+                  <div className="follow">
+                      <button>Follow</button>
+                  </div>
+                  <div className="share-icon-container-center">
+                    <div className="share-icon-container">
+                        <FaAccusoft className="share-icons" />
+                        <FaAccusoft className="share-icons" />
+                        <FaAccusoft className="share-icons" />
+                  </div>
+                  </div>
+                  
+                </div>
                   </div>
                 </div>
                 <p>Tagline: {data.campaign.tagline}</p>
@@ -87,12 +153,12 @@ class CampaignShow extends React.Component {
                 <p>Faq: {data.campaign.faq}</p>
                 <p>Image URL: {data.campaign.image_url}</p>
                 <p>Category: {data.campaign.category}</p>
-                {this.contributions}
                 <p>End Date: {data.campaign.end_date}</p>
               </div>
             );
           }}
         </Query>
+
       </div>
     );
   }
