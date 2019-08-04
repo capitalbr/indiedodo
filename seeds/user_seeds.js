cdb8d5f023426292278e00e8a32870abfb96bf3a
Bradley <bbarnes.me@gmail.com>
2019-08-04 02:57:57 -0500

    Add user-badge to checkout and additional styling/functionality

--- seeds/user_seeds.js
+++ seeds/user_seeds.js
@@ -13,15 +13,16 @@ const User = require('../server/models/User');
 const seedUsers = () => {
   User.deleteMany({}, (err) => { console.log(err) })
   return new Promise((res, rej) => {
-    const userArray = [];
-
-    // userArray.push(
     AuthService.register({
         name: "Albus Dumbledore",
         email: "albus@hogwarts.edu",
         bio_header: "One can never have enough socks.",
         bio: "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.",
         password: "Qwerty1234!"
+    }).then(user => {
+      if (user.name) {
+        console.log(`Success: ${user.name} was created`);
+      }
     })
     AuthService.register({
         name: "Stephen Curry",
@@ -29,6 +30,10 @@ const seedUsers = () => {
         bio_header: "3pt specialist",
         bio: "Slash brothers founding partner",
         password: "12345678"
+    }).then(user => {
+      if (user.name) {
+        console.log(`Success: ${user.name} was created`);
+      }
     })
     AuthService.register({
         name: "Africa Wildlife Foundation",
@@ -36,31 +41,23 @@ const seedUsers = () => {
         bio_header: "Protecting Africa's Most Threatened Species",
         bio: "We’re working to ensure wildlife and wild lands thrive in modern Africa.",
         password: "12345678"
-      })
-      AuthService.register({
-        name: "Amphibian Surival Alliance",
-        email: "info@amphibians.org",
-        bio_header: "Rising To The Challenge Of Conserving Amphibians",
-        bio: "The Amphibian Survival Alliance (ASA) officially launched in 2011, and has since built a committed global alliance of Partners, dedicated to developing a better world for amphibians through coordinated conservation action. ASA promotes and coordinates the implementation of conservation actions for amphibians through an active, growing, engaged, committed, and collaborative partnership around the world. The great need for this endeavour is reflected in the rate of loss of amphibian populations globally, and we face a challenging future as we strive to reverse these trajectories."+
-             "We envisage a future where amphibian conservation is fully collaborative and integrated into global conservation priorities — a future where amphibians are valued for their many contributions to this world, as well as their astonishing beauty and variety.We seek to preserve this variety, as part of healthy ecosystems, for generations to come.",
-        password: "12345678"
+    }).then(user => {
+      if (user.name) {
+        console.log(`Success: ${user.name} was created`);
+      }
     })
-
-    // for (let i = 0, fin = userArray.length; i < fin; i++) {
-    //   const user = userArray[i];
-    //   console.log(user);
-    //   // user.password = "12345678";
-    //   bcrypt.genSalt(10, (err, salt) => {
-    //     bcrypt.hash(user.password, salt, (err, hash) => {
-    //       if (err) throw err;
-    //       user.password_digest = hash;
-    //       user.save().then(user => {
-    //         console.log(`Success: ${user.name} was created`);
-    //       }, err => { console.log(`${user.name} was unable to save due to: ${err}`) })
-    //     })
-    //   })
-    // }
+    AuthService.register({
+      name: "Amphibian Surival Alliance",
+      email: "info@amphibians.org",
+      bio_header: "Rising To The Challenge Of Conserving Amphibians",
+      bio: "The Amphibian Survival Alliance (ASA) officially launched in 2011, and has since built a committed global alliance of Partners, dedicated to developing a better world for amphibians through coordinated conservation action. ASA promotes and coordinates the implementation of conservation actions for amphibians through an active, growing, engaged, committed, and collaborative partnership around the world. The great need for this endeavour is reflected in the rate of loss of amphibian populations globally, and we face a challenging future as we strive to reverse these trajectories."+
+            "We envisage a future where amphibian conservation is fully collaborative and integrated into global conservation priorities — a future where amphibians are valued for their many contributions to this world, as well as their astonishing beauty and variety.We seek to preserve this variety, as part of healthy ecosystems, for generations to come.",
+      password: "12345678"
+    }).then(user => {
+      if (user.name) {
+        console.log(`Success: ${user.name} was created`);
+      }
+    })
   })
 }
-
 seedUsers().then((res) => { mongoose.connection.close() });
