9e13a645c529a65237c4a912615ceac091316817
Josh <josh@burnwatt.io>
2019-08-04 19:44:47 -0500

    Add news styling
    finished news component styling and prepped for master.

--- client/src/components/news/news.jsx
+++ client/src/components/news/news.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import AliceCarousel from 'react-alice-carousel'
-import "react-alice-carousel/lib/alice-carousel.css"
-
+import { FaChevronRight } from "react-icons/fa"
 import { Query } from "react-apollo";
 import Queries from "../../graphql/queries";
 const { GET_NEWS } = Queries;
@@ -10,25 +7,33 @@ const { GET_NEWS } = Queries;
 const News = () => {
   return (
     <div className='news-container'>
-        <h1 classname='news-header'>From the News</h1>
-        <p>Your inside-look at conservation stories across the globe.</p>
-        <Query query={GET_NEWS}>
-          {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error</p>;
-            debugger
-            let randNews = data.getArticles.sort(() => .5 - Math.random());
-            let news = randNews.slice(0,3);
-            return news.map(({ title, description, url, urlToImage }, i) => (
-              <article className='news-article' key={i}>
-                <img className='article-image'src={urlToImage} alt={title}/>
-                <h1 className='article-title'>{title}</h1>
-                <p className='article-description'>{description}</p>
-                <a className='article-link' href={url}>Read Article</a>
-              </article>
-            ));
-          }}
-        </Query>
+        <div className="news-header">
+          <h1>From the News</h1>
+          <p>Your inside-look at conservation stories across the globe.</p>
+        </div>
+        <div className="news-body">
+          <Query query={GET_NEWS}>
+            {({ loading, error, data }) => {
+              if (loading) return <p>Loading...</p>;
+              if (error) return <p>Error</p>;
+              debugger
+              let randNews = data.getArticles.sort(() => .5 - Math.random());
+              let news = randNews.slice(0,3);
+              return news.map(({ title, description, url, urlToImage }, i) => (
+                <article className='news-article' key={i}>
+                  <div className='image-frame'>
+                    <img className='article-image'src={urlToImage} alt={title}/>
+                  </div>
+                  <h1 className='article-title'>{title}</h1>
+                  <p className='article-description'>{description}</p>
+                  <a className='article-link' href={url}>
+                    Read Article >
+                  </a>
+                </article>
+              ));
+            }}
+          </Query>
+        </div>
     </div>
   );
 };
