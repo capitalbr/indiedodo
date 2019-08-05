3c417da1e355981347aa0a0db1a42bc1a7eb807d
Josh <josh@burnwatt.io>
2019-08-04 20:39:17 -0500

    Add banner to landing
    added a simple banner on the landing to separate the campaign list and news sections.

--- client/src/components/landing/landing.jsx
+++ client/src/components/landing/landing.jsx
@@ -30,6 +30,13 @@ class Landing extends React.Component{
                 <div className='recents-container'>
                   <Carousel campaigns={data.campaigns} type='recent' />
                 </div>
+                <div className='banner-container'>
+                  <div className='image-frame'>
+                    <a href="http://lovewildlife.org">
+                      <img src="http://lovewildlife.org/wp-content/uploads/2017/02/Save-the-Loris-Banner-01_web-940x350.png" alt="#savetheloris"/>
+                    </a>
+                  </div>
+                </div>
                 <div className='news-container'>
                   <News />
                 </div>
