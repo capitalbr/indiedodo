adb7ffc84a853f30f6baf36e4f77bd800b88712a
Josh <josh@burnwatt.io>
2019-08-06 12:09:11 -0500

    Update gitignore for keys-dev

---
+++ config/keys_prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY,
+    newsAPI: process.env.NEWS_API,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+}
