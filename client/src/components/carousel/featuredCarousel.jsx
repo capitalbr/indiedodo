61599b1ae60dc5656930393d9d859707199f2c16
Josh <josh@burnwatt.io>
2019-09-05 00:37:47 -0500

    Landing tweaks
    Updated nav, photos, and css for landing page.

--- client/src/components/carousel/featuredCarousel.jsx
+++ client/src/components/carousel/featuredCarousel.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
-import CampaignItem from "../campaigns/CampaignItem";
 import AliceCarousel from 'react-alice-carousel'
-import "react-alice-carousel/lib/alice-carousel.css"
+// import "react-alice-carousel/lib/alice-carousel.css"
 export default class Carousel extends React.Component{
   constructor(props){
     super(props);
