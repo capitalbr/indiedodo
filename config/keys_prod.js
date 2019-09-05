356355e4663e0d43d3d01fcd2b5b55b4527cb814
Bradley <bbarnes.me@gmail.com>
2019-09-05 02:27:08 -0500

    switched aws keys to match what they are in server.js and keys_dev.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,6 +2,6 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
     newsAPI: process.env.NEWS_API,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 }
